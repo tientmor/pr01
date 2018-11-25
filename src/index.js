3ad70da48c0f17d520d4e4c80197290807636da7
Sonic Spec <nhimspec@gmail.com>
2018-11-25 18:04:27 +0700

    Fix action checkauth

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import configureStore, { history } from './store/configureStore';
 import Root from './components/Root';
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 require('./favicon.ico'); // Tell webpack to load favicon.ico
-import { AUTH_CHECK } from './constants/actionTypes';
+import { actCheckAuth } from './actions/authActions';
 
 const store = configureStore();
 
@@ -15,7 +15,7 @@ const token = localStorage.getItem('token');
 
 if (token) {
   // console.log(token);
-  store.dispatch({ type: AUTH_CHECK, token });
+  store.dispatch(actCheckAuth(token));
 }
 
 render(
